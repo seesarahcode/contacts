b41dcde90bfe52917fb012dccfdcaeeb69139e99
psarge2 <paul@psarge.dev>
2022-05-10 12:34:40 -0400

    added registration routes to config, views, and controller. Set up user sign in form with email regex validation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :contacts
   #get 'home/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
 
   # Defines the root path route ("/")
   root "home#index"
