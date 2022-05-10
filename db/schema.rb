8c150c9d21e788b2a68854fa6f42d090be3bb107
psarge2 <paul@psarge.dev>
2022-05-10 12:04:05 -0400

    added a user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_10_133641) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_10_160041) do
   create_table "contacts", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -22,6 +22,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_10_133641) do
     t.string "zip_code"
     t.date "birthday"
     t.string "note"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
