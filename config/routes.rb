27c6f5ec8466154fa411def2ae9e444ab28d4991
psarge2 <paul@psarge.dev>
2022-05-10 12:46:58 -0400

    added sessions for users as they are logged in, created a main index and page for users to travel to wehn logged in, created routes for corresponding navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   resources :contacts
-  #get 'home/index'
+  get 'home/index'
 
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
 
+  delete "logout", to: "sessions#destroy"
 
+
+
+  root to: "main#index"
+  
   # Defines the root path route ("/")
-  root "home#index"
+  # root "home#index"
 end
